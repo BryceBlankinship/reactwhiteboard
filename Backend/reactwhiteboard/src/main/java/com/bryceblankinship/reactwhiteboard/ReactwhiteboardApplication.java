53b2785d6986dc2b0c4b9e9761b1722d53b8914b
Bryce Blankinship <blankinshipbusiness@gmail.com>
2022-09-08 17:52:43 -0400

    imported boilerplate spring backend

---
+++ Backend/reactwhiteboard/src/main/java/com/bryceblankinship/reactwhiteboard/ReactwhiteboardApplication.java
@@ -0,0 +1,13 @@
+package com.bryceblankinship.reactwhiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactwhiteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactwhiteboardApplication.class, args);
+	}
+
+}
---
+++ Backend/reactwhiteboard/src/test/java/com/bryceblankinship/reactwhiteboard/ReactwhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryceblankinship.reactwhiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactwhiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
